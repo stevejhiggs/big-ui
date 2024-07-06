63e267af1306d57cc6e93371a93586d727e69653
Steve Higgs <steve@veed.io>
2024-07-06 18:51:49 +0100

    Add some more components

--- packages/shadcn/src/index.ts
+++ packages/shadcn/src/index.ts
@@ -1,15 +1,21 @@
 // todo, work out why these need explicit extensions
 
+export { Alert } from './components/ui/alert.tsx';
 export { Badge } from './components/ui/badge.tsx';
 
 export {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from './components/ui/card.tsx';
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './components/ui/card.tsx';
 
+export { Checkbox } from './components/ui/checkbox.tsx';
+export { Input } from './components/ui/input.tsx';
+export { Label } from './components/ui/label.tsx';
+export { Progress } from './components/ui/progress.tsx';
 export { Slider } from './components/ui/slider.tsx';
 export { Switch } from './components/ui/switch.tsx';
+export { Textarea } from './components/ui/textarea.tsx';

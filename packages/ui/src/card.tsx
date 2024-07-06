af0e9795d319f90ec9efd2fdd95ddb4f00c7ac65
Steve Higgs <steve@veed.io>
2024-07-06 15:00:52 +0100

    switch to biomejs

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -10,12 +10,7 @@ export function Card({
   href: string;
 }): JSX.Element {
   return (
-    <a
-      className={className}
-      href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"`}
-      rel="noopener noreferrer"
-      target="_blank"
-    >
+    <a className={className} href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"`} rel="noopener noreferrer" target="_blank">
       <h2>
         {title} <span>-&gt;</span>
       </h2>

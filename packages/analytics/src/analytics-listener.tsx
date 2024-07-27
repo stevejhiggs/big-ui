2aefc922967ec367ae14539d6086894b09a3bdf0
Steve Higgs <steve@veed.io>
2024-07-27 13:28:10 +0100

    remove speed insights until I can figure out the hydration error

--- packages/analytics/src/analytics-listener.tsx
+++ packages/analytics/src/analytics-listener.tsx
@@ -1,11 +1,9 @@
 import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export function AnalyticsListener() {
   return (
     <>
       <VercelAnalytics />
-      <SpeedInsights />
     </>
   );
 }

5ffd47ee7d32bb0df94516a80b45f283bb93455b
Steve Higgs <steve@veed.io>
2024-07-27 09:23:22 +0100

    add speed insights

--- packages/analytics/src/analytics-listener.tsx
+++ packages/analytics/src/analytics-listener.tsx
@@ -1,9 +1,11 @@
 import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export function AnalyticsListener() {
   return (
     <>
       <VercelAnalytics />
+      <SpeedInsights />
     </>
   );
 }

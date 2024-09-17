538b472f63cf25c8f551aea3f079bfd71e675fc6
Steve Higgs <steve@veed.io>
2024-09-17 14:44:01 +0100

    upgrade deps

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -23,10 +23,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button
-          appName="docs"
-          className="mx-auto rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-        >
+        <Button className="mx-auto rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44">
           Open alert
         </Button>
       </main>

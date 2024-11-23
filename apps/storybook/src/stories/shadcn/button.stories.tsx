6e0cd37c31218b85ba5b6f19274482997a69351c
Steve Higgs <steve@veed.io>
2024-11-23 11:00:55 +0000

    set composite on typescript projects

--- apps/storybook/src/stories/shadcn/button.stories.tsx
+++ apps/storybook/src/stories/shadcn/button.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
+const meta: Meta<typeof Button> = {
   title: 'ShadCn/Button',
   component: Button,
   parameters: {
@@ -14,7 +14,7 @@ const meta = {
   tags: ['autodocs'],
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
-} satisfies Meta<typeof Button>;
+}
 
 export default meta;
 type Story = StoryObj<typeof meta>;

58c6ad3abcfce81784f50f48d8233cfad31a3853
Steve Higgs <steve@veed.io>
2024-12-31 13:21:57 +0000

    remove the old next based web app

--- apps/web-old/app/protected/page.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export default function Dashboard() {
-  return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <h1>This is a protected page</h1>
-    </div>
-  );
-}
--- apps/web-old/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ['@repo/shadcn'],
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-};
-
-export default nextConfig;

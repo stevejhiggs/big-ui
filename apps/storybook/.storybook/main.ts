29888994227d74231dae7e6054c04995a1604b66
Steve Higgs <stevejhiggs@hotmail.com>
2025-03-04 11:19:36 +0000

    move dashboard out to component

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,5 +1,6 @@
 import { createRequire } from 'node:module';
 import { dirname, join } from 'node:path';
+
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const require = createRequire(import.meta.url);
@@ -8,24 +9,23 @@ const require = createRequire(import.meta.url);
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): any {
+function getAbsolutePath(value: string) {
   return dirname(require.resolve(join(value, 'package.json')));
 }
-const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [getAbsolutePath('@storybook/addon-links'), getAbsolutePath('@storybook/addon-essentials'), getAbsolutePath('@storybook/addon-interactions'), getAbsolutePath('@storybook/addon-themes')],
 
+const config: StorybookConfig = {
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [getAbsolutePath('@storybook/addon-essentials'), getAbsolutePath('@chromatic-com/storybook')],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
-
   typescript: {
     reactDocgen: 'react-docgen-typescript',
   },
-
   docs: {
     autodocs: true,
   },
 };
+
 export default config;
--- apps/storybook/vite.config.ts
+++ apps/storybook/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [tsconfigPaths(), react(), tailwindcss()],
+});
